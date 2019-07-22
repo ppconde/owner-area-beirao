257673626a815272fc025d3e1f8b3f1dad47f486
Pedro Conde <pepconde.1993@gmail.com>
2019-07-22 22:59:35 +0100

    landing page done

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.sendFile("./landing.html", { root: __dirname }));
+app.get("/", (req, res) =>
+  res.sendFile("./static/html/landing.html", { root: __dirname })
+);
 app.get("/*", (req, res) => res.sendFile(req.path, { root: __dirname }));
 
 app.listen(process.env.PORT || 5000);
