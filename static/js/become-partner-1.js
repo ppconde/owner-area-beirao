78595041c56e271b61866877a9e834c9e47296af
Pedro Conde <pepconde.1993@gmail.com>
2019-07-27 14:53:13 +0100

    added more pages

--- static/js/become-partner-1.js
+++ static/js/become-partner-1.js
@@ -1,7 +1,10 @@
+//Add form input animation
 $('[class*="input-"]').focus(function() {
   $($(this).next()).addClass("focused");
 });
 
+//Still not working. it adds the class but it doesn't run the animation
+/*
 $("input").blur(function() {
   var inputValue = $(this).val();
   let nextLabel = $(this).next();
@@ -11,3 +14,11 @@ $("input").blur(function() {
     $(nextLabel).addClass("focused");
   }
 });
+
+//Add color change animation
+$(".button-secondary").click(function() {
+  console.log($(this));
+  $($(this)).addClass(".changecolour-w-y");
+  $(".button-primary").addClass(".changeBckgColour-b-y");
+});
+*/
