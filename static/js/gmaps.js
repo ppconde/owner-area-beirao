d7395aec619f35ed716ef4b1a71b9b602f5d0b7d
Pedro Conde <pepconde.1993@gmail.com>
2019-07-28 20:21:07 +0100

    added card removal

--- static/js/gmaps.js
+++ static/js/gmaps.js
@@ -1,44 +1,62 @@
 function fetchData() {
-  var input = document.getElementById("searchMapInput");
+  //Needs refactoring. Maybe put it in a loop
+  let input1 = document.getElementsByClassName("searchMapInput-1")[0];
+  let input2 = document.getElementsByClassName("searchMapInput-2")[0];
+  let input3 = document.getElementsByClassName("searchMapInput-3")[0];
 
-  var autocomplete = new google.maps.places.Autocomplete(input);
-  autocomplete.addListener("place_changed", function() {
-    var place = autocomplete.getPlace();
+  let autocomplete1 = new google.maps.places.Autocomplete(input1);
+  let autocomplete2 = new google.maps.places.Autocomplete(input2);
+  let autocomplete3 = new google.maps.places.Autocomplete(input3);
+
+  //Adds listener for each input
+  autocomplete1.addListener("place_changed", function() {
+    let place = autocomplete1.getPlace();
+    console.log(this);
 
     //Creates container with the results of place name and address
-    $(".search-container").append(
+    $(".search-container-1").append(
       '<div class="col-desktop-5"><div class="search-results"><h4>' +
         place.name +
         "</h4><p>" +
         place.formatted_address +
-        "</p></div></div>"
+        '</p><a onclick="removeDiv(this)" class="close"></a></div></div>'
     );
-
-    //In case it's needed for later
-    /* Retrieves the following information:
-      - latitude and longiture
-      - opening hours (only for sunday right now)
-      - place rating
-      - price level
-
-
-   /*  document.getElementById(
-      "lat-span"
-    ).innerHTML = place.geometry.location.lat();
-    document.getElementById(
-      "lon-span"
-    ).innerHTML = place.geometry.location.lng();
-    document.getElementById("hours-span").innerHTML =
-      place.opening_hours.weekday_text[0];
-    document.getElementById("ratings-span").innerHTML = place.rating;
-    document.getElementById("price-span").innerHTML = place.price_level;
-
-    $(".search-container").append(`<img src="` + getPicture(place) + `"</img>`);
-
-    document.getElementById(
-      "link-google-maps"
-    ).innerHTML = "Link for Google Maps".link(place.url); */
   });
+
+  autocomplete2.addListener("place_changed", function() {
+    let place = autocomplete2.getPlace();
+    console.log(this);
+
+    //Creates container with the results of place name and address
+    $(".search-container-2").append(
+      '<div class="col-desktop-5"><div class="search-results"><h4>' +
+        place.name +
+        "</h4><p>" +
+        place.formatted_address +
+        '</p><a onclick="removeDiv(this)" class="close"></a></div></div>'
+    );
+  });
+
+  autocomplete3.addListener("place_changed", function() {
+    let place = autocomplete3.getPlace();
+    console.log(this);
+
+    //Creates container with the results of place name and address
+    $(".search-container-3").append(
+      '<div class="col-desktop-5"><div class="search-results"><h4>' +
+        place.name +
+        "</h4><p>" +
+        place.formatted_address +
+        '</p><a onclick="removeDiv(this)" class="close"></a></div></div>'
+    );
+  });
+}
+
+function removeDiv(element) {
+  //Removes search-container
+  element.parentNode.parentNode.parentNode.removeChild(
+    element.parentNode.parentNode
+  );
 }
 
 //Retrieve picture from place
@@ -54,3 +72,28 @@ function getPicture(place) {
     });
 }
  */
+
+//In case it's needed for later
+/* Retrieves the following information:
+        - latitude and longiture
+        - opening hours (only for sunday right now)
+        - place rating
+        - price level
+  
+  
+     /*  document.getElementById(
+        "lat-span"
+      ).innerHTML = place.geometry.location.lat();
+      document.getElementById(
+        "lon-span"
+      ).innerHTML = place.geometry.location.lng();
+      document.getElementById("hours-span").innerHTML =
+        place.opening_hours.weekday_text[0];
+      document.getElementById("ratings-span").innerHTML = place.rating;
+      document.getElementById("price-span").innerHTML = place.price_level;
+  
+      $(".search-container").append(`<img src="` + getPicture(place) + `"</img>`);
+  
+      document.getElementById(
+        "link-google-maps"
+      ).innerHTML = "Link for Google Maps".link(place.url); */
